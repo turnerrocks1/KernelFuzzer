cda2471d2334661f93582507c34df1ac277a8d26
Turnerhackz1 <turner881189@gmail.com>
2022-08-03 18:55:35 -0500

    Create fukkk.c

---
+++ fukkk.c
@@ -0,0 +1,246 @@
+//
+//  main.c
+//  turnerrhackzfuzzer
+//
+//  Created by Booty Warrior on 8/3/22.
+//
+
+#include <stdint.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <IOKit/IOKitLib.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+
+void listFiles(const char *path);
+
+/*void raceconditiontemplate() {
+    
+}*/
+
+int maybe(){
+  static int seeded = 0;
+  if(!seeded){
+    srand(time(NULL));
+    seeded = 1;
+  }
+  return !(rand() % 100);
+}
+
+void flip_bit(void* buf, size_t len){
+  if (!len)
+    return;
+  size_t offset = rand() % len;
+  ((uint8_t*)buf)[offset] ^= (0x01 << (rand() % 8));
+}
+
+kern_return_t
+fake_IOConnectCallMethod(
+  mach_port_t connection,
+  uint32_t    selector,
+  uint64_t   *input,
+  uint32_t    inputCnt,
+  void       *inputStruct,
+  size_t      inputStructCnt,
+  uint64_t   *output,
+  uint32_t   *outputCnt,
+  void       *outputStruct,
+  size_t     *outputStructCntP)
+{
+    int watcher = 0;
+    int looplimit = 100000; //2147483647; lets set the limit to something less time consuming like 100000?
+  /*if (maybe()){
+    flip_bit(input, sizeof(*input) * inputCnt);
+  }*/
+
+  /*if (maybe()){
+    flip_bit(inputStruct, inputStructCnt);
+  }*/
+    
+for (;;) { //run the fuzzer in an endless loop cycle :)
+    watcher++;
+    if (watcher == looplimit) {
+        break;
+    }
+    printf("here : %d", watcher);
+    IOConnectCallMethod(
+    connection,
+    selector,
+    input,
+    inputCnt,
+    inputStruct,
+    inputStructCnt,
+    output,
+    outputCnt,
+    outputStruct,
+    outputStructCntP);
+    //lets call this first with original parameters ...
+    flip_bit(input, sizeof(*input) * inputCnt);
+    flip_bit(inputStruct, inputStructCnt);
+    
+}
+    return IOConnectCallMethod(
+    connection,
+    selector,
+    input,
+    inputCnt,
+    inputStruct,
+    inputStructCnt,
+    output,
+    outputCnt,
+    outputStruct,
+    outputStructCntP);
+    
+}
+
+void fuzzXD(io_name_t class, uint32_t num) {
+    //lets declare some interesting int's shall we ;)
+    printf("about to fuzz %s\n", class);
+    int interesting[16];
+    interesting[0] = 0x41414141;
+    interesting[1] = 0x13371337;
+    interesting[2] = 0;
+    interesting[3] = -1;
+    interesting[4] = 1;
+    interesting[5] = 65535;
+    interesting[6] = -65535;
+    
+    kern_return_t kr;
+    io_iterator_t iterator = IO_OBJECT_NULL;
+    io_connect_t connect = MACH_PORT_NULL;
+    kr = IOServiceGetMatchingServices(kIOMainPortDefault, IOServiceMatching(class), &iterator);
+    io_service_t service = IOIteratorNext(iterator);
+    kr = IOServiceOpen(service, mach_task_self(), num, &connect);
+    //fuzzying like this will probably reach use after frees, type confusions, and BoFs, and oobs
+    //but I don't think it hit race condition sceneraios so let's add that feature after a call to
+    //the fake IOConnectCallMethod!
+    for (uint32_t sel = 0; sel < 40; sel++){
+    for (int inter = 0; inter < 7; inter++){
+    //When reversing IOKit drivers on iOS ive never seen a method selector past at most 18;
+    //I don't know how much more it is for macOS kexts so let's try a wild guess of 40 ;)
+    //also flip bit is going to flip input  and inputStruct so might as well squeeze as much as possible
+    //;)
+    uint64_t inputScalar[1];
+    uint32_t inputScalarCnt = 1;
+
+    inputScalar[0] = interesting[inter];
+    //inputScalar[1] = 0;
+    
+    char inputStruct[16];
+    size_t inputStructCnt = 0;
+
+    uint64_t outputScalar[16];
+    outputScalar[1] = interesting[inter];
+    uint32_t outputScalarCnt = 1;
+
+    char outputStruct[16];
+    outputStruct[0] = (char)interesting[inter];
+    size_t outputStructCnt = 1;
+    kern_return_t err = fake_IOConnectCallMethod(connect, sel, inputScalar, inputScalarCnt, inputStruct, inputStructCnt, outputScalar, &outputScalarCnt, outputStruct, &outputStructCnt);
+        if(err != KERN_SUCCESS) {
+            printf("fuzzying class %s failed :(",class);
+        }
+        printf("done fuzzying and flipping %s connectcallmethod bits",class);
+    }
+    }
+    
+}
+int pickkexts() {
+        kern_return_t kr;
+        io_iterator_t iterator = IO_OBJECT_NULL;
+    kr = IOServiceGetMatchingServices(kIOMainPortDefault, IOServiceMatching("IOService"), &iterator);
+        uint32_t type;
+        for (;;) {
+            io_service_t service = IOIteratorNext(iterator);
+            if (service == IO_OBJECT_NULL) {
+                break;
+            }
+            io_name_t class_name = {};
+            IOObjectGetClass(service, class_name);
+            uint64_t entry_id = 0;
+            IORegistryEntryGetRegistryEntryID(service, &entry_id);
+            //printf("%s 0x%llx  ", class_name, entry_id);
+            
+            io_connect_t connect = MACH_PORT_NULL;
+            for (type = 0; type < 0x200; type++) {
+                kr = IOServiceOpen(service, mach_task_self(), type, &connect);
+                if (kr == KERN_SUCCESS) {
+                    goto can_open;
+                }
+            }
+            for (type = 0xffffff00; type != 0; type++) {
+                kr = IOServiceOpen(service, mach_task_self(), type, &connect);
+                if (kr == KERN_SUCCESS) {
+                    goto can_open;
+                }
+            }
+            uint32_t types[] = { 0x61736864, 0x484944, 0x99000002, 0xFF000001, 0x64506950, 0x6C506950, 0x88994242, 0x48494446, 0x48494444, 0x57694669 };
+            uint32_t count = sizeof(types) / sizeof(types[0]);
+            for (uint32_t type_idx = 0; type_idx < count; type_idx++) {
+                type = types[type_idx];
+                kr = IOServiceOpen(service, mach_task_self(), type, &connect);
+                if (kr == KERN_SUCCESS) {
+                    goto can_open;
+                }
+            }
+            //printf("\n");
+            goto next;
+            //break;
+        can_open:
+            //return 2;
+            fuzzXD(class_name,type);
+            //printf("Can open %s with type %d\n",class_name,type);
+        next:;
+        }
+    return 0;
+}
+
+/*char* listed[100000] = {''};
+void listFiles(const char *path)
+{
+    int increment = 0;
+    struct dirent *dp;
+    DIR *dir = opendir(path);
+
+    // Unable to open directory stream
+    if (!dir)
+        return;
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        ++increment;
+        //printf("%s\n", dp->d_name);
+        listed[increment] = (char)dp->d_name;
+        //return (void)listed;
+        //((char)dp->d_name + "\n");
+    }
+
+    // Close directory stream
+    closedir(dir);
+}
+
+void getextensionslist() {
+    char* extensionpath = "/System/Library/Extensions";
+    listFiles(extensionpath);
+    
+}*/
+//I was told not to go about listing every extension from /System/Library/Extensions and just use
+//Bazad's IOService iterator Method.
+
+int main(int argc, const char * argv[]) {
+    printf("this is a multi-platform IOKit Fuzzer developed by turnerhackz1 on 0x8/0x3/0x2022 XD\n");
+    
+    /*kern_return_t ret = fake_IOConnectCallMethod(<#mach_port_t connection#>, <#uint32_t selector#>, <#uint64_t *input#>, <#uint32_t inputCnt#>, <#void *inputStruct#>, <#size_t inputStructCnt#>, <#uint64_t *output#>, <#uint32_t *outputCnt#>, <#void *outputStruct#>, <#size_t *outputStructCntP#>)*/
+    //getextensionslist();
+    int ret = pickkexts();
+    //printf("%s listed : ", (char*)listed);
+    
+    return ret;
+}
+main();
